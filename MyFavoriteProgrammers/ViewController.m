ce4855fac69cd7d70149be3a8646591992b01ce7
Ginny Fahs <ginny@uber.com>
2019-01-29 13:14:21 -0800

    Rotate 4 views in tandem

---
+++ MyFavoriteProgrammers/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MyFavoriteProgrammers
+//
+//  Created by Ginny Fahs on 1/28/19.
+//  Copyright © 2019 Ginny's Games. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
